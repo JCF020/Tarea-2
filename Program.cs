c7009cf1965f8475858d9ded4fe800cd97eabb79
JCF020 <joseangel687@hotmail.com>
2020-02-07 18:55:21 -0400

    Ejercicio Armstrong

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Numero Armstrong  del 1 al 500: ");
+
+            int a, b, c, d;
+           
+            for (int numero = 1; numero <= 500; numero++)
+            {
+                a = numero / 100;
+                b = (numero - a * 100) / 10;
+                c = (numero - a * 100 - b * 10);
+                d = a * a * a + b * b * b + c * c * c;
+                if (numero == d)
+                {
+                    Console.WriteLine($"{numero}");
+                }
+
+
+            }
+            Console.ReadKey();
+        }
+            
+    }
+}
